e0f5231d7decea83df9ab1667838cb668f0e954f
pkarmstr <pkarmstr@gmail.com>
2014-03-27 23:49:52 -0400

    Initial commit

---
+++ model_one.py
@@ -0,0 +1,16 @@
+__author__ = 'keelan'
+
+import numpy
+
+class TranslationModel:
+
+    def __init__(self, aligned_sentences):
+        self.aligned_sentences = aligned_sentences
+        self.e_alphabet = None
+        self.f_alphabet = None
+
+    def convert_to_vector(self):
+        pass
+
+    def init_translation_table(self):
+        pass
---
+++ reader.py
@@ -0,0 +1,5 @@
+__author__ = 'keelan'
+
+def parallel_corpus_reader(f1, f2):
+    with open(f1, "r") as ef_in, open(f2, "r") as ff_in:
+        pass
