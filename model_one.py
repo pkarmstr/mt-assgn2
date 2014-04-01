4888382ef725f44796fd18c0d9d77d4c00958e61
pkarmstr <pkarmstr@gmail.com>
2014-04-01 04:30:10 -0400

    got em working

--- model_one.py
+++ model_one.py
@@ -3,6 +3,8 @@ __author__ = 'keelan'
 import numpy
 from helper import Alphabet
 from reader import parallel_corpus_reader
+from nltk.model.ngram import NgramModel
+from nltk.probability import LidstoneProbDist
 
 class TranslationModel:
 
@@ -33,18 +35,19 @@ class TranslationModel:
         self.t_table = numpy.zeros([self.e_alphabet.size(), self.f_alphabet.size()])
         self.t_table.fill(.25)
 
-    def expectation_maximization(self, iterations=10):
+    def expectation_maximization(self, iterations=20):
         for i in range(iterations):
-            print "iteration {:d}".format(i)
+            print "iteration {:d}".format(i+1)
             # initialize
-            self.counts = numpy.zeros([self.e_alphabet.size(), self.f_alphabet.size()])
-            self.s_total = numpy.zeros(self.e_alphabet.size())
             self.total = numpy.zeros(self.f_alphabet.size())
+            self.counts = numpy.zeros([self.e_alphabet.size(), self.f_alphabet.size()])
 
             for e_instance,f_instance in self.aligned_sentences:
+                self.s_total = numpy.zeros(self.e_alphabet.size())
+
                 # compute normalization
                 for e_word in e_instance.data:
-                    self.s_total[e_word] = numpy.sum(self.t_table[e_word, f_instance.data])
+                    self.s_total[e_word] += numpy.sum(self.t_table[e_word, f_instance.data])
 
                 # collect counts
                 for e in e_instance.data:
@@ -53,9 +56,10 @@ class TranslationModel:
                         self.total[f] += self.t_table[e,f]/self.s_total[e]
 
             # estimate probabilities
-            for f in f_instance.data:
-                for e in e_instance.data:
+            for f in range(self.f_alphabet.size()):
+                for e in range(self.e_alphabet.size()):
                     self.t_table[e,f] = self.counts[e,f]/self.total[f]
+
 
 
     def train(self):
@@ -63,11 +67,34 @@ class TranslationModel:
         self.init_translation_table()
         self.expectation_maximization()
 
+        """
         for i in range(self.e_alphabet.size()):
             for j in range(self.f_alphabet.size()):
                 print self.e_alphabet.get_label(i),
                 print self.f_alphabet.get_label(j),
                 print self.t_table[i,j]
+        """
+
+    def evaluate(self, candidate_data):
+        pass
+
+    def sentence_translation_probability(self, e_sentence, f_sentence):
+        """need to optimize"""
+        e_sentence = self.convert_to_sparse_vector(e_sentence, lang="e")
+        f_sentence = self.convert_to_sparse_vector(f_sentence, lang="f")
+        product = 1.
+        for e in e_sentence:
+            sent_sum = 0
+            for f in f_sentence:
+                sent_sum += self.t_table[e,f]
+            product *= sent_sum
+        return product/(len(f_sentence+1)**len(e_sentence))
+
+    def build_language_model(self):
+        all_text = [word for e_instance,f_instance in self.aligned_sentences \
+                    for word in e_instance.raw_data[:-1]]
+        est = lambda fdist, bins: LidstoneProbDist(fdist, 0.2)
+        self.lm = NgramModel(3, all_text, estimator=est)
 
 if __name__ == "__main__":
     import argparse
@@ -81,3 +108,4 @@ if __name__ == "__main__":
     data = parallel_corpus_reader(args.english, args.foreign, max_sents=1000)
     tm = TranslationModel(data)
     tm.train()
+    print tm.sentence_translation_probability("the book".split(), "das buch".split())
--- reader.py
+++ reader.py
@@ -1,20 +1,47 @@
 __author__ = 'keelan'
 
 import codecs
+from collections import defaultdict,namedtuple
 from nltk.tokenize import WordPunctTokenizer
 from helper import Instance
 
 TOKENIZER = WordPunctTokenizer()
+CandidateTranslations = namedtuple("CandidateTranslations", ["source_sent", "candidates"])
 
 def parallel_corpus_reader(f1, f2, max_sents=100):
     total_seen = 0
     result = []
     with codecs.open(f1, "r", "utf-8") as ef_in, codecs.open(f2, "r", "utf-8") as ff_in:
-        while total_seen <= max_sents:
-            result.append(map(prepare_sentence, [ef_in.readline(), ff_in.readline()]))
+        for line in ef_in:
+            result.append(map(prepare_sentence, [line, ff_in.readline()]))
             total_seen += 1
+            if total_seen >= max_sents:
+                break
     return result
+
+def eval_data_reader(source, target):
+    candidate_translations = defaultdict(list)
+    source_sentences = {}
+    all_data = []
+    with codecs.open(source, "r", "utf-8") as f_in:
+        for line in f_in:
+            id_,sent = line.rstrip().split("\t")
+            source_sentences[id_] = TOKENIZER.tokenize(sent)
+
+    with codecs.open(target, "r", "utf-8") as f_in:
+        for line in f_in:
+            id_,sent = line.rstrip().split("\t")
+            candidate_translations[id_].append(TOKENIZER.tokenize(sent))
+
+    for id_,source_sent in source_sentences.iteritems():
+        candidates = candidate_translations[id_]
+        all_data.append(CandidateTranslations(source_sent, candidates))
+
+    return all_data
+
+
 
 def prepare_sentence(sent):
     toks = [s.lower() for s in TOKENIZER.tokenize(sent)]
+    toks.append("__NULL__")
     return Instance(raw_data=toks)
