3bea2666ac3e97bbae31a1cd9d4ce669be1a01f6
keelan <pkarmstr@gmail.com>
2014-04-02 23:22:13 -0400

    giving good number

--- language_model.py
+++ language_model.py
@@ -1,26 +1,51 @@
 __author__ = 'keelan'
 
 import numpy
+import sys
 
 class BigramModel:
+
+    LOG_EPSILON = numpy.log(numpy.finfo(float).eps)
 
     def __init__(self, data, word_alphabet):
         self.data = data
         self.word_alphabet = word_alphabet
         self.word_alphabet.add("__START__")
         self.start_id = self.word_alphabet.get_index("__START__")
-        self.unigram_counts = numpy.zeros(self.word_alphabet.size())
         self.bigram_counts = numpy.zeros([self.word_alphabet.size(), self.word_alphabet.size()])
 
     def build_counts(self):
         for sentence in self.data:
-            self.unigram_counts[sentence] += 1
             self.bigram_counts[self.start_id, sentence[0]] += 1
             self.bigram_counts[sentence[:-1], numpy.roll(sentence, -1)[:-1]] += 1
 
-    def build_probabilities(self):
-        self.unigram_probabilities = self.unigram_counts/numpy.sum(self.unigram_counts)
-        self.bigram_probabilities = self.bigram_probabilities/numpy.sum(self.bigram_counts, axis=0)
+        # add-one smoothing
+        self.bigram_counts += 1
 
-    def log_prob(self):
-        pass
+    def build_probabilities(self):
+        self.bigram_probabilities = self.bigram_counts/numpy.sum(self.bigram_counts, axis=0)
+
+    def train(self):
+        self.build_counts()
+        self.build_probabilities()
+
+    def convert_to_vector(self, tokenized_sentence):
+        vector = numpy.zeros(len(tokenized_sentence)+1, dtype=int)
+        vector[0] = self.start_id
+        for i,word in enumerate(tokenized_sentence):
+            try:
+                vector[i+1] = self.word_alphabet.get_index(word)
+            except KeyError:
+                vector[i+1] = -1
+        return vector
+
+    def log_prob(self, tokenized_sentence):
+        lp = 0.
+        vector = self.convert_to_vector(tokenized_sentence)
+        for i in xrange(len(vector)-1):
+            j = i+1
+            if vector[i] == -1 or vector[j] == -1:
+                lp += self.LOG_EPSILON
+            else:
+                lp += numpy.log(self.bigram_probabilities[vector[i],vector[j]])
+        return lp
--- model_one.py
+++ model_one.py
@@ -1,23 +1,39 @@
+# coding=utf-8
 __author__ = 'keelan'
 
 import numpy
+import time
 from helper import Alphabet
-from reader import parallel_corpus_reader
+from reader import parallel_corpus_reader, TOKENIZER
 from nltk.model.ngram import NgramModel
 from nltk.probability import LidstoneProbDist
+from language_model import BigramModel
 
 class TranslationModel:
+    ETA = .1
 
     def __init__(self, aligned_sentences):
         self.aligned_sentences = aligned_sentences
         self.e_alphabet = Alphabet()
         self.f_alphabet = Alphabet()
 
-    def convert_to_sparse_vector(self, raw_data, lang="e"):
+    def convert_to_vector(self, raw_data, lang="e", training=True):
         if lang == "e":
-            return numpy.array(map(self.e_alphabet.get_index, raw_data), dtype=int)
+            alphabet = self.e_alphabet
+        else:
+            alphabet = self.f_alphabet
 
-        return numpy.array(map(self.f_alphabet.get_index, raw_data), dtype=int)
+        if training:
+            return numpy.array(map(alphabet.get_index, raw_data), dtype=int)
+
+        else:
+            vector = numpy.zeros(len(raw_data))
+            for i,word in enumerate(raw_data):
+                try:
+                    vector[i] = alphabet.get_index(word)
+                except KeyError:
+                    continue #ignoring OOV words for now
+            return vector
 
     def populate_alphabets(self):
         for e_instance,f_instance in self.aligned_sentences:
@@ -28,16 +44,19 @@ class TranslationModel:
             for i,token in enumerate(f_instance.raw_data):
                 self.f_alphabet.add(token)
 
-            e_instance.data = self.convert_to_sparse_vector(e_instance.raw_data, "e")
-            f_instance.data = self.convert_to_sparse_vector(f_instance.raw_data, "f")
+            e_instance.data = self.convert_to_vector(e_instance.raw_data, "e")
+            f_instance.data = self.convert_to_vector(f_instance.raw_data, "f")
 
     def init_translation_table(self):
         self.t_table = numpy.zeros([self.e_alphabet.size(), self.f_alphabet.size()])
+        self.previous_t_table = numpy.zeros([self.e_alphabet.size(), self.f_alphabet.size()])
         self.t_table.fill(.25)
 
     def expectation_maximization(self, iterations=20):
-        for i in range(iterations):
-            print "iteration {:d}".format(i+1)
+        i = 0
+        while not self.has_converged():
+            time1 = time.time()
+            print "iteration {:d}".format(i+1),
             # initialize
             self.total = numpy.zeros(self.f_alphabet.size())
             self.counts = numpy.zeros([self.e_alphabet.size(), self.f_alphabet.size()])
@@ -51,21 +70,32 @@ class TranslationModel:
 
                 # collect counts
                 for e in e_instance.data:
-                    for f in f_instance.data:
-                        self.counts[e,f] += self.t_table[e,f]/self.s_total[e]
-                        self.total[f] += self.t_table[e,f]/self.s_total[e]
+                    #for f in f_instance.data:
+                    tmp = self.t_table[e,f_instance.data]/self.s_total[e]
+                    self.counts[e,f_instance.data] += tmp
+                    self.total[f_instance.data] += tmp
 
             # estimate probabilities
-            for f in range(self.f_alphabet.size()):
-                for e in range(self.e_alphabet.size()):
-                    self.t_table[e,f] = self.counts[e,f]/self.total[f]
+            self.t_table = self.counts/self.total
 
+            i += 1
 
+            print "\t {:f} seconds".format(time.time()-time1)
+
+    def has_converged(self): # need to fix this
+        delta = numpy.sum(numpy.abs(self.t_table - self.previous_t_table))
+        self.previous_t_table = numpy.copy(self.t_table)
+        print "delta", delta
+        if delta < self.ETA:
+            return True
+
+        return False
 
     def train(self):
         self.populate_alphabets()
         self.init_translation_table()
         self.expectation_maximization()
+        self.build_language_model()
 
         """
         for i in range(self.e_alphabet.size()):
@@ -78,23 +108,29 @@ class TranslationModel:
     def evaluate(self, candidate_data):
         pass
 
-    def sentence_translation_probability(self, e_sentence, f_sentence):
+    def t_table_log_prob(self, e_sentence, f_sentence):
         """need to optimize"""
-        e_sentence = self.convert_to_sparse_vector(e_sentence, lang="e")
-        f_sentence = self.convert_to_sparse_vector(f_sentence, lang="f")
+        e_sentence = self.convert_to_vector(e_sentence, lang="e", training=False)
+        f_sentence = self.convert_to_vector(f_sentence, lang="f", training=False)
         product = 1.
         for e in e_sentence:
             sent_sum = 0
             for f in f_sentence:
                 sent_sum += self.t_table[e,f]
             product *= sent_sum
-        return product/(len(f_sentence)**len(e_sentence))
+        return numpy.log(product/(len(f_sentence)**len(e_sentence)))
 
     def build_language_model(self):
-        all_text = [word for e_instance,f_instance in self.aligned_sentences \
-                    for word in e_instance.raw_data[:-1]]
-        est = lambda fdist, bins: LidstoneProbDist(fdist, 0.2)
-        self.lm = NgramModel(3, all_text, estimator=est)
+        all_data = []
+        for e_sentence,_ in self.aligned_sentences:
+            all_data.append(e_sentence.data)
+        self.language_model = BigramModel(all_data, self.e_alphabet)
+        self.language_model.train()
+
+    def translation_log_prob(self, e_sentence, f_sentence):
+        e_sentence = TOKENIZER.tokenize(e_sentence)
+        f_sentence = TOKENIZER.tokenize(f_sentence)
+        return self.t_table_log_prob(e_sentence, f_sentence) + self.language_model.log_prob(e_sentence)
 
 if __name__ == "__main__":
     import argparse
@@ -105,7 +141,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    data = parallel_corpus_reader(args.english, args.foreign, max_sents=1000)
+    data = parallel_corpus_reader(args.english, args.foreign, max_sents=100)
     tm = TranslationModel(data)
     tm.train()
-    print tm.sentence_translation_probability("the book".split(), "das buch".split())
+
+    print tm.translation_log_prob("the book", "el libro")
+    print tm.translation_log_prob("book the", "el libro")
+    print tm.translation_log_prob("you are quite right", "tiene toda la razón del mundo")
+    print tm.translation_log_prob("you are quite wrong", "tiene toda la razón del mundo")
